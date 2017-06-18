0a55935c418c051fe04671a1f4965409c2540edc
Phil Wilson <pjwilson27@gmail.com>
2017-06-18 23:18:43 +0000

    Think I got it?

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -1,15 +1,24 @@
 module Messaging
-    def get_messages(page_number = 0)
-        response = self.class.get('/message_threads', headers: { "authorization" => @auth_token })
+    def get_messages(page_number)
+        if page_number >= 0
+            response = self.class.get('/message_threads', body: { "page": "#{page_number}" }, headers: { "authorization" => @auth_token })
+        else page_number <=0 || nil
+            response = self.class.get('/message_threads', headers: {"authorization" => @auth_token })
+        end
         
         JSON.parse(response.body).to_a
     end
     
-    def create_message
-        if message_token
-            response = self.class.post('/messages', body: {"sender": sender, "receipient_id": receipient_id, "token": message_token, "subject": subject, "stripped-text": stripped-text })
-        else
-            response = self.class.post('/message', body: {"sender": sender, "receipient_id": receipient_id, "subject": subject, "stripped-text": stripped-text })
-        end
+    def create_message(sender, receipient_id, token, subject, stripped)
+        response = self.class.post('/messages', body: {
+            "sender": sender, 
+            "receipient_id": receipient_id, 
+            "token": token, 
+            "subject": subject, 
+            "stripped-text": stripped
+            
+        },
+        
+        headers: {"authorization" => @auth_token })
     end
 end
