07b9d3b534424cec1ae82910d47a5177872e3bed
Phil Wilson <pjwilson27@gmail.com>
2017-06-18 16:32:02 +0000

    Completed CP3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,14 @@
 require 'httparty'
+require 'json'
 
 class Kele
     
     include HTTParty
     
+    base_uri "https://www.bloc.io/api/v1/"
+    
     def initialize(email, password)
-        response = self.class.post('https://www.bloc.io/api/v1/', body: {
+        response = self.class.post("/sessions", body: {
             "email": email,
             "password": password
         })
@@ -13,6 +16,12 @@ class Kele
         raise 'Incorrect Email or Password...Try again.' if response.code != 200
         
         @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        
+        JSON.parse(response.body)
     end
     
 end
